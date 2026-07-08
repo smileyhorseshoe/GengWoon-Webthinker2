015b79d55a23257d03dcaca582f06578c8a1b259
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-08 08:42:56 +0000

    Jul 8, 2026, 8:42 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,14 +3,14 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
 
-  ball = new Sprite(400, 200, 200, 200);
+  // ball = new Sprite(400, 200, 200, 200);
 
   // Basic shape testing
   // write your codes here
   fill("yellow")
   stroke("black")
   strokeweight(10)
-  circle()
+  circle(400,200,100)
   // End Basic shape testing
 
    // Create a bouncing ball sprite
