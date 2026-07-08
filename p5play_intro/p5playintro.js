91748f5239bb36943e514d2363c4a0929f3e870d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-08 08:43:56 +0000

    Jul 8, 2026, 8:43 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -9,7 +9,7 @@ function setup() {
   // write your codes here
 fill("yellow")
 stroke("black")
-strokeweight(10)
+strokeWeight(10)
 circle(400,200,100)
 Rect(600,300,100,100)
   // End Basic shape testing
