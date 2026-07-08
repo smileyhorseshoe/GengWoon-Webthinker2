2e7f703e3b5bb79be932fc9d0a6da6a31d13c835
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-08 08:31:47 +0000

    Jul 8, 2026, 8:31 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,8 +3,8 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
 
-  ball = new.Sprite();
-  
+  ball = new Sprite();
+
   // Basic shape testing
   // write your codes here
   
