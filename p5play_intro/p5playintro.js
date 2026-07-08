46abf879cab88656887bcc6165866d0dab0fd16a
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-08 08:52:45 +0000

    Jul 8, 2026, 8:52 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -13,7 +13,7 @@ strokeWeight(10)
 // circle(400,70,100)
 // rect(350,150,100,200)
 // triangle(50,50,400,200,50,350)
-quad()
+quad(400,)
   // End Basic shape testing
 
    // Create a bouncing ball sprite
