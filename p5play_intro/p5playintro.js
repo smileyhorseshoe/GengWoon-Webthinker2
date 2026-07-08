24567e07bd39575191714bc6d8f0d2c3a26cf53d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-08 08:56:44 +0000

    Jul 8, 2026, 8:56 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,22 +3,22 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
 
-  // ball = new Sprite(400, 200, 200, 200);
-
+  
   // Basic shape testing
   // write your codes here
-// fill("yellow")
-// stroke("black")
-// strokeWeight(10)
-// circle(400,70,100)
-// rect(350,150,100,200)
-// triangle(50,50,400,200,50,350)
-// quad(400,50,700,350,400,250,100,350)
+  // fill("yellow")
+  // stroke("black")
+  // strokeWeight(10)
+  // circle(400,70,100)
+  // rect(350,150,100,200)
+  // triangle(50,50,400,200,50,350)
+  // quad(400,50,700,350,400,250,100,350)
   // End Basic shape testing
-
-   // Create a bouncing ball sprite
-   // write your codes here
-
+  
+  // Create a bouncing ball sprite
+  // write your codes here
+  
+  // ball = new Sprite(400, 200, 300, 300);
 }
 
 function draw() {
