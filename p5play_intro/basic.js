94f2ca1bcf5f1b798f9075211f8342276a3b68df
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-15 08:01:15 +0000

    Jul 15, 2026, 8:01 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -27,7 +27,7 @@ function setup() {
   let y = 200
 for (let i = 0; i <= 20; i+=2) {
   console.log(i)
-  text("First 10 Even Numbers" + x, 100)
+  text("First 10 Even Numbers" + 50, 50)
   text(i, x, y)
   x = x + 50
 }
