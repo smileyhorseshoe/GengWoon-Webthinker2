ac848610b67e71a732553f458caa8e4fb230b948
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-15 07:44:19 +0000

    Jul 15, 2026, 7:44 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -7,7 +7,7 @@ function setup() {
  let a = 1;
  let b = 2;
  let sum = (a+b) * (a+b) / b;
- console.log("A is " + a)
+ console.log("A has a value of " + a + " while B has a value of " + b)
  console.log(sum)
   // --- Exercise: Area of Triangle ---
   // write your codes here
