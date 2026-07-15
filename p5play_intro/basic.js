b99ff8db610b92bd8e2959f82dde304da42405f6
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-15 08:06:53 +0000

    Jul 15, 2026, 8:06 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -33,7 +33,7 @@ for (let i = 0; i <= 20; i+=2) {
   sum = sum +i
 }
   console.log("Sum of first 10 even numbers " + sum )
-  text("Sum of first 10 even numbers:" + sum, 400, 300)
+  text("Sum of first 10 even numbers:" + sum, 0, 100)
   // --- Exercise: Age category classification ---
   // write your codes here
 
