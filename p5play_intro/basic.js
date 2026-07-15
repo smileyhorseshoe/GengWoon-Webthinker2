5482c33740947194f1aae36d53959caceca31226
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-15 07:54:29 +0000

    Jul 15, 2026, 7:54 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -17,7 +17,10 @@ function setup() {
   let area = 0.5 * base * height 
   console.log("The area of the triangle which has a base of " + base + " and a height of " + height +" has the area of" + area);
 // para for string is string, x, y e.g. ()
-text("Area: ")
+text("Area: " + area, 400,200)
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
 
