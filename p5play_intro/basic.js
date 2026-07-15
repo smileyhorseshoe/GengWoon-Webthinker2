afdbe7f7776316794fdff9fc8a6d8c95528c17c6
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-15 08:30:42 +0000

    Jul 15, 2026, 8:30 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -67,7 +67,7 @@ groceries.push("orange")
 groceries.push("butter")
 groceries.shift(); // remove first item
 groceries.pop(); // remove last item
+groceries.splice(1,1)
 console.log(groceries)
-
 }
 
