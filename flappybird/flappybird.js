127bf78da7e9ad27b9b48dc2d1c52d5c505056bb
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-22 08:58:48 +0000

    Jul 22, 2026, 8:58 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -29,7 +29,7 @@ function setup() {
     textSize(14);
     text("vel.y" + bird.vel.y.toFixed(2),10,20); 
     text("isMoving:" + bird.isMOving, 10 ,40);
-    console.log("sleeping:" + bird.sleeping,10,60)
+    text("sleeping:" + bird.sleeping,10,60)
     // bird.mass = 2;  how heavy, heavier = affected by gravity motr
     bird.drag = 0.02; // air resistance, higher = more resistantce
     bird.bounciness = 1; // higher = bouncier
