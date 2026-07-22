fa1317ca756e9c72c6676a871219ff401ab04f66
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-22 07:55:01 +0000

    Jul 22, 2026, 7:55 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -5,6 +5,7 @@ let floor;
 // Image assets
 let birdMidImg = loadImage("assets/yellowbird-midflap.png") 
 let day = loadImage("assets/background-day.png")
+let night = loadImage("flappybird/assets/background-night.png")
 function preload(){
     
 }
