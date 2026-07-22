92efe4cfdaaf77476072401643bc54842853d437
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-22 08:48:36 +0000

    Jul 22, 2026, 8:48 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -45,7 +45,7 @@ function setup() {
 function draw() {
     image(day,0,0,width,height); // image u drawing, x, y, width, height
 
-    if (kb.presses("space") || button.mouse.presed("left")) {
+    if (kb.presses("space") || mouse.preses("left")) {
         bird.vel.y = -5; // up velocity
     }
 }
