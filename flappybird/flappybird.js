1da7fa170644c7558dd7180b343972962d73c46d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-22 08:19:25 +0000

    Jul 22, 2026, 8:19 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -24,7 +24,7 @@ function setup() {
     bird.width=30;
     bird.height=30;
 
-    world.gravity.y = 10;
+    world.gravity.y = 1.625;
     bird.mass = 2;
     bird.drag = 0.02;
     bird.bounciness = 0.5;
