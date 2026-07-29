fcc40e00fd5282ba9b1d98c9c787c264301fb0df
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-29 08:32:10 +0000

    Jul 29, 2026, 8:32 AM

--- flappybird/playground/flappybird_03.js
+++ flappybird/playground/flappybird_03.js
@@ -20,7 +20,7 @@ function preload(){
     flapUpImg = loadImage ("assets/yellowbird-upflap.png");
     flapDownImg = loadImage("assets/yellowbird-downflap.png");
     pipe = loadImage("assets/pipe-green.png");
-    let gamoe
+    let gameoverimg = loadImage
 }
 
 
