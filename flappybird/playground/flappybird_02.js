2028f42254ae329008860d0e5fe61e6bdc2ca9be
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-29 08:02:54 +0000

    Jul 29, 2026, 8:02 AM

--- flappybird/playground/flappybird_02.js
+++ flappybird/playground/flappybird_02.js
@@ -83,5 +83,7 @@ function spawnPipePair() {
 
     bottomPipe = new Sprite(400,midY + gap/2 + 200,52,320,'static');
     bottomPipe.img = pipe;
-    
+
+    pipeGroup.add(bottomPipe);
+    pipeGroup.layer
 }
