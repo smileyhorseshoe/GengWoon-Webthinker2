e0942fe8a08a6dd8228f4d0a15d904c85b8fe154
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-29 08:22:16 +0000

    Jul 29, 2026, 8:22 AM

--- flappybird/playground/flappybird_03.js
+++ flappybird/playground/flappybird_03.js
@@ -97,7 +97,7 @@ function spawnPipePair() {
     pipeGroup.add(bottomPipe);
     pipeGroup.layer= 0// go behind other sprites
 
-    topPipe = new Sprite(400,midY -gap/2-200,52,320,'static');
+    topPipe = new Sprite(bird.x + 400,midY -gap/2-200,52,320,'static');
     topPipe.img = pipe;
     topPipe.rotation = 180;
     pipeGroup.add(topPipe);
