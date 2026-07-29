ce1e5ae30f3c7883d5b4d673bee600b1ef78080d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-07-29 07:42:15 +0000

    Jul 29, 2026, 7:42 AM

--- flappybird/playground/flappybird_02.js
+++ flappybird/playground/flappybird_02.js
@@ -50,8 +50,8 @@ function draw() {
         bird.sleeping =false
         bird.vel.y = -5; // up velocity
     // debug info
+}
 text("vel.y" + bird.vel.y.toFixed(2),10,20); // text,x,y
 text("isMoving:" + bird.isMOving, 10 ,40);
 text("sleeping:" + bird.sleeping,10,60)
-    }
 }
