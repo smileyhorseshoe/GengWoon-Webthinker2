8e1d13617d0e94ada47786b769e6ae20f5a486a1
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-08-05 08:45:45 +0000

    Aug 5, 2026, 8:45 AM

--- flappybird/playground/flappybird_04.js
+++ flappybird/playground/flappybird_04.js
@@ -145,9 +145,9 @@ function drawScore(x,y,score,digitWidth,digitHeight) {
   // loop through each digit
   for (let i = 0; i < scoreStr.length; i++) {
     // gets number digit from the score string (e.g. "4" or "2")
-    let digit= int(scoreString[i]);
+    let digit= int(scoreStr[i]);
     //  x-position of this digit, next character will move right
-    let xPos = startX + i* digitWidth;
+    let xPos = startx + i* digitWidth;
     //create a new sprite the size of the digit image
     let digitSprite = new scoreDigits.Sprite(xPos,y,digitWidth,digitHeight);
     // get the digit image from the array based on placement order which corresponds to the digit
