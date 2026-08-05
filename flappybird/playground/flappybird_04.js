32ae0cce90b05b9e9af7076257f64252c05eb76b
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-08-05 08:49:49 +0000

    Aug 5, 2026, 8:49 AM

--- flappybird/playground/flappybird_04.js
+++ flappybird/playground/flappybird_04.js
@@ -141,7 +141,7 @@ function drawScore(x,y,score,digitWidth,digitHeight) {
   // total width taken up by all digits
   let totalWidth = scoreStr.length*digitWidth;
   // starting x coords
-  let Startx= x - totalWidth /2;
+  let startX= x - totalWidth /2;
   // loop through each digit
   for (let i = 0; i < scoreStr.length; i++) {
     // gets number digit from the score string (e.g. "4" or "2")
