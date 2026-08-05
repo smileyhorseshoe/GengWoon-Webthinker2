ad993374dac15a140a7f31f717675a7d255b8251
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-08-05 08:40:09 +0000

    Aug 5, 2026, 8:40 AM

--- flappybird/playground/flappybird_04.js
+++ flappybird/playground/flappybird_04.js
@@ -121,7 +121,8 @@ function draw() {
     // increase score if pipe passed
     for (let pipe of pipeGroup) {
       let pipeRightEdge = pipe.x+pipe.w /2
-      
+      // center pos - half bird width = right edge pos
+        
     }
   }
 
