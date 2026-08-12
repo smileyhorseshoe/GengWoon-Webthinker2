2c89ca8d865e87544b17938bf4e6b03f175a623c
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-08-12 08:19:08 +0000

    Aug 12, 2026, 8:19 AM

--- flappybird/playground/flappybird_05.js
+++ flappybird/playground/flappybird_05.js
@@ -133,7 +133,9 @@ function draw() {
           score = 0;
           startGame = false;
           pipes.removeAll() 
-          
+          bird.vel.x = 0;
+          bird.vel.y = 0;
+          b
         },3000)
       
         }
