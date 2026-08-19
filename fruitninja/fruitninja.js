c833173748a929ae423f6935c9d3264205bdd541
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-08-19 07:50:42 +0000

    Aug 19, 2026, 7:50 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -1,1 +1,15 @@
 // write your codes here
+let background;
+
+function preload(){
+    background = loadImage('assets/dojobackground.png');
+}
+
+function setup() {
+  new Canvas(800,600);
+  
+}
+
+function draw() {
+    image(background,0,0,width,height);
+}
