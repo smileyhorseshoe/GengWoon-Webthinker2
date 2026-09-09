1cacf5a9bbf5fdfb337fbf2c6a4ee948c6e81926
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-09 08:11:24 +0000

    Sep 9, 2026, 8:11 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -107,7 +107,7 @@ function splitFruit(x, y, fruitData) {
     left.rotationSpeed = -5;
     left.life = 60; // remove after 30 frames
     left.collider = "dynamic"; // dynamic -- yes gravity and yes collision / static - no gravity and yes collision / none - no gravity and no collision
-
+    left.overlaps()
     // create right half
     let right = new fruitHalves.Sprite(x + 10, y, 40, 40);
     right.img = fruitData.half2;
