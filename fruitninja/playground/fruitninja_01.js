0a72785685cadb16d0be3508ceb16c4dce1b9141
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-09 08:14:35 +0000

    Sep 9, 2026, 8:14 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -118,5 +118,5 @@ function splitFruit(x, y, fruitData) {
     right.rotationSpeed = 5;
     right.life = 60; // remove after 30 frames
     right.collider = "dynamic"; // dynamic -- yes gravity and yes collision / static - no gravity and yes collision / none - no gravity and no collision
-    right.overlaps(allSprites); // disable collision 
+    right.overlaps(allSprites); // disable collision =)
 }
