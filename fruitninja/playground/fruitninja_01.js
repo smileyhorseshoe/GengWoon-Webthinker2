2081cf374145492d67066262b9a26f036cc7950f
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-09 09:01:11 +0000

    Sep 9, 2026, 9:01 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -45,10 +45,11 @@ function preload() {
     }
     let apple = {
         whole: loadImage("assets/apple1.png"),
-        half1: loadImage("assets/apple2.png")
+        half1: loadImage("assets/apple2.png"),
+        half2: loadImage("assets/apple2.png"),
     }
     // store the fruit objects into an array
-    fruitTypes = [peach, watermelon,strawberry,corn];
+    fruitTypes = [peach, watermelon,strawberry,corn,dragonfruit,kiwi,apple];
 
 }
 
