d8f447efae25bc46f97319293d24dfea36728fac
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 07:35:49 +0000

    Sep 16, 2026, 7:35 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -78,7 +78,7 @@ function draw() {
     text("frameCount:"+ frameCount, 10, 40);
 
     // call spawnFruit function
-    if (frameCount % 20 == 0) {
+    if (frameCount % 1 == 0) {
         // 60 frames =1 second
         spawnFruit();
     }
