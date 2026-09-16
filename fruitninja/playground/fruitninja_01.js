397313de149ffaf495d707d7674599b8f2836fd6
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 08:55:05 +0000

    Sep 16, 2026, 8:55 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -198,7 +198,8 @@ function draw() {
     image(background, 0, 0, width, height); // image(image, x, y, width, height)
     // draw depending on gameState
     if(gameState === "start"){
-        fill("rgb(100,255,100")
+        fill("rgb(100,255,100") // text colour
+        textSize(100)
         // start menu
         if (kb.presses(" ") || mouse.presses()) {
             gameState = "playing"
