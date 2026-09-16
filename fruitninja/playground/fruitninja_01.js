34306bc90a63c5660760ee2ecd0b5da49035f429
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 07:54:47 +0000

    Sep 16, 2026, 7:54 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -4,6 +4,7 @@ let fruitGroup; // group for whole fruits
 let fruitTypes  = []; // store fruit image objects
 let trail;
 let fruitHalves; // new group for sliced halves
+let score;
 
 function preload() {
     // Load image
