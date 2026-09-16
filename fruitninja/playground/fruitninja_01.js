ad06c55871f3346cdc3b4abdd4b6e02f0fdaf29f
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 08:04:34 +0000

    Sep 16, 2026, 8:04 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -135,7 +135,7 @@ function draw() {
     text("frameCount:"+ frameCount, 10, 40);
     fill("rgb(0,255,0)")
     text("Score:"+ score, 10, 100);
-    fill("")
+    fill("rgba")
     text("Missed: " + missed,10,100)
 
 
