079d83bd5f3c3f6cb33abde01e48758f4bd17b51
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 07:40:42 +0000

    Sep 16, 2026, 7:40 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -56,7 +56,7 @@ function preload() {
     let garlic = {}
     // store the fruit objects into an array
     fruitTypes = [peach, watermelon,strawberry,corn,dragonfruit,kiwi,apple,orange];
-
+    // add scaleMod
 }
 
 function setup() {
