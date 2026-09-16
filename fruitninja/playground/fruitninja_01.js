30cbbd3a954cd61bee4d4d4174904bbaf8310bf7
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 08:54:17 +0000

    Sep 16, 2026, 8:54 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -171,8 +171,11 @@ function preload() {
         scaleMod:2.5,
     }
     let mangoSteen = {
-        whole:loadImage("assets/mangosteen1.png")
-        half1:loadImage("assets/mangosteen2.png")
+        whole:loadImage("assets/mangosteen1.png"),
+        half1:loadImage("assets/mangosteen2.png"),
+        half2:loadIMage("assets/mangosteen3.png"),
+        // add scaleMod
+        scaleMod:2.5,
     }
 
     // store the fruit objects into an array
