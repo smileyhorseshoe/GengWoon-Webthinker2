21fb04a37a649b3e873d470b2c90433f8586e198
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 08:43:06 +0000

    Sep 16, 2026, 8:43 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -6,7 +6,7 @@ let trail;
 let fruitHalves; // new group for sliced halves
 let score=0;
 let missed = 0;
-let gameState="playing"; // "start", "playing", "gameover"
+let gameState="start"; // "start", "playing", "gameover"
 function preload() {
     // Load image
     background = loadImage("assets/dojobackground.png");
