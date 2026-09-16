761c36287d016b7ccf42d158ef384ea9eaf6b1dc
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 08:42:36 +0000

    Sep 16, 2026, 8:42 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -141,13 +141,6 @@ function preload() {
         // add scaleMod
         scaleMod:2.5,
     }
-    let paparika = {
-       whole:loadImage("assets/paparika1.png"),
-       half1:loadImage("assets/paparika3.png"),
-       half2:loadImage("assets/paparika3.png"),
-    //    add scaleMod
-       scaleMod:2.5
-    }
 
     // store the fruit objects into an array
     fruitTypes = [peach, watermelon,strawberry,corn,dragonfruit,kiwi,apple,orange,garlic,lemon,lime,mango,pear,potato,pumpkin,tomato,banana,papaya,paparika];
