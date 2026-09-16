782ae6754a08f906734359329b0582d083553977
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 08:58:06 +0000

    Sep 16, 2026, 8:58 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -166,7 +166,7 @@ function preload() {
     let redOnion = {
         whole:loadImage("assets/red_onion1.png"),
         half1:loadImage("assets/red_onion3.png"),
-        half2:loadImage("assets/redonion3.png"),
+        half2:loadImage("assets/red_onion3.png"),
         // add scaleMod
         scaleMod:2.5,
     }
