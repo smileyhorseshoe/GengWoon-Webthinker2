56b8236773d46a09fe12b877fd95ec78eb8ce408
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 09:05:56 +0000

    Sep 16, 2026, 9:05 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -191,7 +191,7 @@ function preload() {
         //add scaleMod
         scaleMod:2.5,
     }
-
+// reminder to self - add avocado
     // store the fruit objects into an array
     fruitTypes = [peach, watermelon,strawberry,corn,dragonfruit,kiwi,apple,orange,garlic,lemon,lime,mango,pear,potato,pumpkin,tomato,banana,papaya,coconut,fig,onion,redOnion,mangoSteen,starfruit,cherry];
     
