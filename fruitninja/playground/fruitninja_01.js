29ba4e8d1f4de878ba21d324ad8fc71ed64de99f
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-16 09:00:41 +0000

    Sep 16, 2026, 9:00 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -220,9 +220,11 @@ function draw() {
     // fill("#dadada") // Text colour
     // textSize(50);
     // text("frameCount:"+ frameCount, 10, 40);
+    textAlign(LEFT, BOTTOM) // default alignment
+
     textSize(50)
     fill("rgb(0,255,0)")
-    text("Score:"+ score, 10, 40);
+    text("Score:"+ score, 10, 50);
     fill("rgba(136,9,9,0.5") // ("rgba(red,green,blue,alpba)") alpha=  transparency the lower the number the more transparent it is. fully opaque is one can also use slider to control
     text("Missed: " + missed,10,100)
 
