e307e789908d3d25110a430a3286a961f3312b15
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-23 08:10:19 +0000

    Sep 23, 2026, 8:10 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -8,7 +8,8 @@ let score=0;
 let missed = 0;
 let gameState="start"; // "start", "playing", "win" , lose
 let gameStartTime = 0;
-
+let gameTimer = 0;
+let 
 function preload() {
     // Load image
     background = loadImage("assets/dojobackground.png");
