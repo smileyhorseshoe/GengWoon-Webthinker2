1e9a5969f832bfbe361ec5334d3745efa12bfb20
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-23 08:39:08 +0000

    Sep 23, 2026, 8:39 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -294,7 +294,7 @@ function draw() {
             gameState = "win";
     
         }
-        else if (missed >=25) {
+        else if (missed >=5) {
             gameState = "lose"
         }
     }
