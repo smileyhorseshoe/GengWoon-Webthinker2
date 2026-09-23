4d6db23c358a401f2719fc8057c857e30e55012c
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-23 08:34:57 +0000

    Sep 23, 2026, 8:34 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -284,7 +284,7 @@ function draw() {
     }
     
     gameTimer = floor((millis() - gameStartTime)/1000)
-    text("Time: " + gameTimer, width/2,60);
+    text("Time: " + gameTimer - gameDuration, width/2,60);
     // ceil(1.1) - round up
     // .toFixed(2) - nearest decimal place  
     // win / lose condition
