a1c7db64f9947688147e5ae9f0e091cbf8fe62f1
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-23 08:05:19 +0000

    Sep 23, 2026, 8:05 AM

--- fruitninja/playground/fruitninja_01.js
+++ fruitninja/playground/fruitninja_01.js
@@ -298,7 +298,7 @@ function draw() {
     textAlign(CENTER,CENTER);
     text("Game Over!", width/2, height/2, height * 0.3)
     text("You Lose!", width/2, height/2, height * 0.5)
-    text("Score: "+ score, width)
+    text("Score: "+ score, width/2,height/2)
     return;
 }
 
